dc2cf7b91c3da2a489352045c16b837fae7b3d41
HYUNJOON KWAK <deartomefirst@gmail.com>
2023-10-20 02:39:12 +0900

    chore: add prepare commit message

---
+++ src/components/InvoiceInputModal.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function InvoiceInputModal() {
+  return <div>송장 입력 모달</div>;
+}
+
+export default InvoiceInputModal;
---
+++ src/components/PaymentConfirmModal.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function PaymentConfirmModal() {
+  return <div>결제 확인 모달</div>;
+}
+
+export default PaymentConfirmModal;
