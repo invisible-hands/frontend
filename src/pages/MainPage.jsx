3dbe15f8483467acdb003e6d5b8707c3b0313e53
GHeeJeon <jhjeon_99@naver.com>
2023-10-31 16:22:46 +0900

    feat: add Hot item preview

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ImageSlider from '../components/ImageSlider';
 import slidesData from '../data/slidesData';
+import HotPreview from '../components/HotPreview';
 
 function MainPage() {
   return (
-    <div className="max-w-screen-lg mx-auto">
+    <div className="whitespace-nowrap max-w-screen-lg mx-auto">
       <ImageSlider slides={slidesData} />
+      <HotPreview />
     </div>
   );
 }
