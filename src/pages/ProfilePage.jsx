406b052cac60f8aa607aaba57a2f2f8c62522733
GHeeJeon <jhjeon_99@naver.com>
2023-11-10 04:42:57 +0900

    feat: add nickname, address, account request

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -3,7 +3,7 @@ import { TERipple } from 'tw-elements-react';
 import DaumPostcode from 'react-daum-postcode';
 import axios from 'axios';
 import { FaTimes } from 'react-icons/fa';
-import profileImg from '../assets/logo-square.png';
+import profileImg from '../assets/bettingground.png';
 
 function ProfilePage() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -14,14 +14,29 @@ function ProfilePage() {
   const [postcode, setPostcode] = useState('');
   const [address, setAddress] = useState('');
   const [addressDetail, setAddressDetail] = useState('');
+  const [bankName, setBankName] = useState('');
   const [bankAccount, setBankAccount] = useState('');
   const [virtualMoney, setVirtualMoney] = useState('');
   const [agreedToTerms, setAgreedToTerms] = useState(false);
 
   const [nicknameError, setNicknameError] = useState('');
   const [addressDetailError, setAddressDetailError] = useState('');
+  const [bankAccountError, setBankAccountError] = useState('');
 
   const [isUpdatingNickname, setIsUpdatingNickname] = useState(false);
+  const [isUpdatingAddress, setIsUpdatingAddress] = useState(false);
+  const [isUpdatingAccount, setIsUpdatingAccount] = useState(false);
+
+  const bankOptions = ['국민은행', '농협은행', '신한은행'];
+
+  const openAddressModal = () => {
+    setIsModalOpen(true);
+  };
+
+  // 모달 닫기
+  const closeAddressModal = () => {
+    setIsModalOpen(false);
+  };
 
   const validateNickname = value => {
     if (value === '') {
@@ -39,17 +54,34 @@ function ProfilePage() {
     return '';
   };
 
+  const validateBankAccount = value => {
+    // 빈 문자열인 경우
+    if (value.trim() === '') {
+      return '계좌번호를 비워둘 수 없습니다.';
+    }
+
+    // 숫자만 포함하는 정규식
+    const regex = /^\d+$/;
+    if (!regex.test(value)) {
+      // 숫자만 포함하지 않는 경우 오류 메시지 반환
+      return '계좌번호는 숫자만 포함할 수 있습니다.';
+    }
+
+    // 유효한 계좌번호인 경우
+    return '';
+  };
+
   useEffect(() => {
     // Mock data
     const mockData = {
       name: '김경매',
       nickname: '!',
-      profileImage: 'image_data_placeholder',
-      bankAccount: '국민은행 1234-5678-9101',
-      roadName: '서울특별시 양천구 목동중앙북로 1길',
-      addressName: '서울특별시 양천구 123-4',
-      zipcode: 19524,
-      detailAddress: '1011호',
+      profileImage: profileImg,
+      bankAccount: '',
+      roadName: '',
+      addressName: '',
+      zipcode: '',
+      detailAddress: '',
       email: 'betting@gmail.com',
       virtualMoney: 20000,
     };
@@ -58,7 +90,7 @@ function ProfilePage() {
     setName(mockData.name);
     setNickname(mockData.nickname);
     setEmail(mockData.email);
-    // setProfileImage(mockData.profileImage); // 주의: 실제 이미지 처리 방식에 따라 다를 수 있습니다.
+    // setProfileImage(mockData.profileImage);
     setAddress(mockData.roadName);
     setAddressDetail(mockData.detailAddress);
     setBankAccount(mockData.bankAccount);
@@ -69,6 +101,15 @@ function ProfilePage() {
     const errorMessage = validateNickname(mockData.nickname);
     setNicknameError(errorMessage);
   }, []);
+
+  const handleNicknameChange = e => {
+    const { value } = e.target;
+    setNickname(value); // 닉네임 상태 업데이트
+
+    // 유효성 검사를 통해 에러 메시지 업데이트
+    const errorMessage = validateNickname(value);
+    setNicknameError(errorMessage);
+  };
 
   const handleAddressDetailChange = e => {
     const { value } = e.target;
@@ -83,29 +124,19 @@ function ProfilePage() {
     }
   };
 
-  const handleNicknameChange = e => {
+  const handleBankAccountChange = e => {
     const { value } = e.target;
-    setNickname(value); // 닉네임 상태 업데이트
-
-    // 유효성 검사를 통해 에러 메시지 업데이트
-    const errorMessage = validateNickname(value);
-    setNicknameError(errorMessage);
+    setBankAccount(value); // 계좌번호 상태 업데이트
+    const errorMessage = validateBankAccount(value);
+    setBankAccountError(errorMessage); // 에러 메시지 상태 업데이트
   };
 
   // 모달 열기
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  // 모달 닫기
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
 
   const handleSelectAddress = data => {
     setAddress(data.address);
     setPostcode(data.zonecode);
-    closeModal();
+    closeAddressModal();
   };
 
   const toggleTermsAgreement = () => {
@@ -129,7 +160,7 @@ function ProfilePage() {
   };
 
   const updateNickname = async () => {
-    if (isUpdatingNickname) return; // Prevent multiple requests
+    if (isUpdatingNickname) return;
     setIsUpdatingNickname(true);
 
     try {
@@ -137,15 +168,52 @@ function ProfilePage() {
         'https://ka1425de5708ea.user-app.krampoline.com/api/user/nickname',
         { nickname },
       );
-      // Handle response here
       console.log(response.data);
-      // Optionally, set some state to show success message
     } catch (error) {
-      // Handle error here
       console.error(error);
-      // Optionally, set some state to show error message
     } finally {
       setIsUpdatingNickname(false);
+    }
+  };
+
+  const updateAddress = async () => {
+    if (isUpdatingAddress || !postcode || !address || !addressDetail) return; // 요청을 방지하는 조건들
+    setIsUpdatingAddress(true);
+
+    const addressData = {
+      roadName: address,
+      addressName: address,
+      zipcode: parseInt(postcode, 10),
+      detailAddress: addressDetail,
+    };
+
+    try {
+      const response = await axios.put(
+        'https://ka1425de5708ea.user-app.krampoline.com/api/user/address',
+        addressData,
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsUpdatingAddress(false);
+    }
+  };
+
+  const updateAccountInfo = async () => {
+    if (!bankName || !bankAccount || isUpdatingAccount) return;
+    setIsUpdatingAccount(true);
+
+    try {
+      const response = await axios.put(
+        'https://ka1425de5708ea.user-app.krampoline.com/api/user/account',
+        { bankName, bankAccount },
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsUpdatingAccount(false);
     }
   };
 
@@ -158,7 +226,7 @@ function ProfilePage() {
             <button
               type="button"
               className="text-deepblue1 self-end m-3"
-              onClick={closeModal}
+              onClick={closeAddressModal}
             >
               <FaTimes size={20} />
             </button>
@@ -181,7 +249,7 @@ function ProfilePage() {
             <img
               src={profileImg}
               alt="프로필 이미지"
-              className="w-32 h-32 rounded-full my-2 bg-deepblue1 border-solid border-2 border-blue2"
+              className="w-32 h-32 rounded-full my-2 border-solid border-2 border-blue2"
             />
           )}
           <input
@@ -220,7 +288,7 @@ function ProfilePage() {
               nicknameError || !nickname ? 'bg-grayish' : 'bg-deepblue2'
             } text-white px-2 py-1 rounded`}
           >
-            {isUpdatingNickname ? '수정 중...' : '수정'}
+            {isUpdatingNickname ? '저장 중...' : '저장'}
           </button>
           {nicknameError && (
             <p className="text-red-500 text-xs">{nicknameError}</p>
@@ -260,12 +328,14 @@ function ProfilePage() {
             <button
               type="button"
               className="bg-deepblue2 text-white px-2 py-1 rounded ml-2"
-              onClick={openModal}
+              onClick={openAddressModal}
             >
               검색
             </button>
           </TERipple>
         </div>
+
+        {/* 상세 주소 입력란 */}
         <div className="mb-2">
           <input
             type="text"
@@ -273,23 +343,69 @@ function ProfilePage() {
             value={addressDetail}
             onChange={handleAddressDetailChange}
             placeholder="상세 주소 입력"
-            className={`truncate mb-2 px-2 py-1 rounded border-2 ${
+            className={`truncate flex-1 mb-2 px-2 py-1 rounded border-2 ${
               addressDetailError ? 'border-red-500' : 'border-gray-300'
             }`}
           />
+          <button
+            type="button"
+            disabled={
+              !postcode || !address || !addressDetail || isUpdatingAddress
+            }
+            onClick={updateAddress}
+            className={`ml-2 ${
+              !postcode || !address || !addressDetail
+                ? 'bg-grayish'
+                : 'bg-deepblue2'
+            } text-white px-2 py-1 rounded`}
+          >
+            {isUpdatingAddress ? '저장 중...' : '저장'}
+          </button>
           {addressDetailError && (
             <p className="text-red-500 text-xs">{addressDetailError}</p>
           )}
         </div>
+        {/* 계좌번호 및 은행 선택 드롭다운 */}
         <div className="mb-2">
+          <select
+            value={bankName}
+            onChange={e => setBankName(e.target.value)}
+            className="bg-grayish text-deepblue2 px-2 py-1 rounded mr-2"
+          >
+            <option value="">은행 선택</option>
+            {bankOptions.map(option => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
           <input
             type="text"
             id="bankAccount"
             value={bankAccount}
-            onChange={e => setBankAccount(e.target.value)}
+            onChange={handleBankAccountChange}
             placeholder="계좌번호"
-            className="mb-2 px-2 py-1 rounded border-2 border-gray-300"
+            className={`flex-1 px-2 py-1 rounded border-2 ${
+              bankAccountError ? 'border-red-500' : 'border-gray-300'
+            }`}
           />
+          <button
+            type="button"
+            onClick={updateAccountInfo}
+            disabled={
+              !bankAccount || !bankName || isUpdatingAccount || bankAccountError
+            } // 에러가 있을 때 버튼 비활성화
+            className={`ml-2 ${
+              !bankAccount || !bankName || bankAccountError
+                ? 'bg-gray-300 cursor-not-allowed'
+                : 'bg-deepblue2'
+            } text-white px-2 py-1 rounded`}
+          >
+            {isUpdatingAccount ? '저장 중...' : '저장'}
+          </button>
+          {bankAccountError && (
+            <p className="text-red-500 text-xs mb-2 py-1">{bankAccountError}</p> // 에러 메시지 출력
+          )}
         </div>
         <div className="mb-2">
           <input
@@ -331,7 +447,7 @@ function ProfilePage() {
           <button
             type="submit"
             disabled={!isFormValid()}
-            className={`bg-blue-500 text-white px-4 py-2 rounded ${
+            className={`bg-deepblue2 text-white px-4 py-2 rounded ${
               !isFormValid() ? 'opacity-50 cursor-not-allowed' : ''
             }`}
           >
