dd3a931069a251ad4f514a881008a4597f2bc5f6
GHeeJeon <jhjeon_99@naver.com>
2023-11-14 20:24:30 +0900

    feat: update main page with response

--- src/components/preview/HotPreview.jsx
+++ src/components/preview/HotPreview.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { BsFillArrowRightCircleFill as RightArrowIcon } from 'react-icons/bs';
 import { useNavigate } from 'react-router-dom';
-import sampleImg from '../../assets/sample.png';
 import detailBackground from '../../assets/detailBackground.jpg';
+import ItemDetail from '../ItemDetail';
 
 function HotPreview() {
   const navigate = useNavigate();
+  const [hotItems, setHotItems] = useState([]);
+
+  useEffect(() => {
+    const fetchHotItems = async () => {
+      try {
+        const response = await axios.get(
+          'https://ka1425de5708ea.user-app.krampoline.com/api/auction?page=0&size=3&sort=view,desc',
+        );
+        setHotItems(response.data.data.items);
+      } catch (error) {
+        console.error('Error fetching hot items:', error);
+      }
+    };
+
+    fetchHotItems();
+  }, []);
 
   const handleRightArrowClick = () => {
     navigate('/hot');
+  };
+
+  const goToProductDetail = productId => {
+    navigate(`/product/${productId}`);
   };
 
   return (
@@ -17,72 +38,19 @@ function HotPreview() {
         인기상품
       </div>
       <div className="grid grid-cols-4 gap-4">
-        {/* 상품1 */}
-        <div className="border p-4 rounded-md relative">
-          {/* relative for positioning the timer */}
-          <div className="absolute top-1 right-1 bg-deepblue2/20 text-deepblue1 text-md px-2 py-1 rounded">
-            00:59:30
-          </div>
-          {/* 타이머 */}
-          <img
-            src={sampleImg}
-            alt="상품 이미지1"
-            className="w-full h-48 object-cover mb-4 rounded-md"
+        {hotItems.map(item => (
+          <ItemDetail
+            key={item.auctionId}
+            item={item}
+            onClick={() => goToProductDetail(item.auctionId)}
           />
-          <div className="text-xl font-bold mb-2 truncate">상품명1</div>
-          <div className="text-md mb-1">
-            현재 입찰 <span className="text-lg font-bold">800</span> 원
-          </div>
-          <div className="text-md font-bold text-deepblue1">
-            즉시구매 <span className="text-lg font-bold">1000</span> 원
-          </div>
-        </div>
-        {/* 상품2 */}
-        <div className="border p-4 rounded-md relative">
-          {/* relative for positioning the timer */}
-          <div className="absolute top-1 right-1 bg-deepblue2/20 text-deepblue1 text-md px-2 py-1 rounded">
-            00:45:15
-          </div>
-          {/* 타이머 */}
-          <img
-            src={sampleImg}
-            alt="상품 이미지2"
-            className="w-full h-48 object-cover mb-4 rounded-md"
-          />
-          <div className="text-xl font-bold mb-2 truncate">상품명2</div>
-          <div className="text-md mb-1">
-            현재 입찰 <span className="text-lg font-bold">800</span> 원
-          </div>
-          <div className="text-md font-bold text-deepblue1">
-            즉시구매 <span className="text-lg font-bold">1000</span> 원
-          </div>
-        </div>
-        {/* 상품3 */}
-        <div className="border p-4 rounded-md relative">
-          {/* relative for positioning the timer */}
-          <div className="absolute top-1 right-1 bg-deepblue2/20 text-deepblue1 text-md px-2 py-1 rounded">
-            00:45:15
-          </div>
-          {/* 타이머 */}
-          <img
-            src={sampleImg}
-            alt="상품 이미지3"
-            className="w-full h-48 object-cover mb-4 rounded-md"
-          />
-          <div className="text-xl font-bold mb-2 truncate">상품명3</div>
-          <div className="text-md mb-1">
-            현재 입찰 <span className="text-lg font-bold">800</span> 원
-          </div>
-          <div className="text-md font-bold text-deepblue1">
-            즉시구매 <span className="text-lg font-bold">1000</span> 원
-          </div>
-        </div>
-        {/* 상품4 */}
+        ))}
+        {/* 더보기 상품 */}
         <div className="border p-4 rounded-md relative">
           <div className=" blur-sm">
             {/* relative for positioning the timer */}
             <div className="absolute top-1 right-1 bg-deepblue2/20 text-deepblue1 text-md px-2 py-1 rounded">
-              00:45:15
+              99:01:07
             </div>
             {/* 타이머 */}
             <img
--- src/components/preview/NewPreview.jsx
+++ src/components/preview/NewPreview.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { BsFillArrowRightCircleFill as RightArrowIcon } from 'react-icons/bs';
 import { useNavigate } from 'react-router-dom';
-import sampleImg from '../../assets/sample.png';
 import detailBackground from '../../assets/detailBackground.jpg';
+import ItemDetail from '../ItemDetail';
 
 function NewPreview() {
   const navigate = useNavigate();
+  const [newItems, setNewItems] = useState([]);
+
+  useEffect(() => {
+    const fetchNewItems = async () => {
+      try {
+        const response = await axios.get(
+          'https://ka1425de5708ea.user-app.krampoline.com/api/auction?page=0&size=3&sort=latest,asc',
+        );
+        setNewItems(response.data.data.items);
+      } catch (error) {
+        console.error('Error fetching new items:', error);
+      }
+    };
+    fetchNewItems();
+  }, []);
+
+  const goToProductDetail = productId => {
+    navigate(`/product/${productId}`);
+  };
 
   const handleRightArrowClick = () => {
     navigate('/new');
@@ -16,72 +36,19 @@ function NewPreview() {
         신병받아라
       </div>
       <div className="grid grid-cols-4 gap-4">
-        {/* 상품1 */}
-        <div className="border p-4 rounded-md relative">
-          {/* relative for positioning the timer */}
-          <div className="absolute top-1 right-1 bg-deepblue2/20 text-deepblue1 text-md px-2 py-1 rounded">
-            00:59:30
-          </div>
-          {/* 타이머 */}
-          <img
-            src={sampleImg}
-            alt="상품 이미지1"
-            className="w-full h-48 object-cover mb-4 rounded-md"
+        {newItems.map(item => (
+          <ItemDetail
+            key={item.auctionId}
+            item={item}
+            onClick={() => goToProductDetail(item.auctionId)}
           />
-          <div className="text-xl font-bold mb-2 truncate">상품명1</div>
-          <div className="text-md mb-1">
-            현재 입찰 <span className="text-lg font-bold">800</span> 원
-          </div>
-          <div className="text-md font-bold text-deepblue1">
-            즉시구매 <span className="text-lg font-bold">1000</span> 원
-          </div>
-        </div>
-        {/* 상품2 */}
-        <div className="border p-4 rounded-md relative">
-          {/* relative for positioning the timer */}
-          <div className="absolute top-1 right-1 bg-deepblue2/20 text-deepblue1 text-md px-2 py-1 rounded">
-            00:45:15
-          </div>
-          {/* 타이머 */}
-          <img
-            src={sampleImg}
-            alt="상품 이미지2"
-            className="w-full h-48 object-cover mb-4 rounded-md"
-          />
-          <div className="text-xl font-bold mb-2 truncate">상품명2</div>
-          <div className="text-md mb-1">
-            현재 입찰 <span className="text-lg font-bold">800</span> 원
-          </div>
-          <div className="text-md font-bold text-deepblue1">
-            즉시구매 <span className="text-lg font-bold">1000</span> 원
-          </div>
-        </div>
-        {/* 상품3 */}
-        <div className="border p-4 rounded-md relative">
-          {/* relative for positioning the timer */}
-          <div className="absolute top-1 right-1 bg-deepblue2/20 text-deepblue1 text-md px-2 py-1 rounded">
-            00:45:15
-          </div>
-          {/* 타이머 */}
-          <img
-            src={sampleImg}
-            alt="상품 이미지3"
-            className="w-full h-48 object-cover mb-4 rounded-md"
-          />
-          <div className="text-xl font-bold mb-2 truncate">상품명3</div>
-          <div className="text-md mb-1">
-            현재 입찰 <span className="text-lg font-bold">800</span> 원
-          </div>
-          <div className="text-md font-bold text-deepblue1">
-            즉시구매 <span className="text-lg font-bold">1000</span> 원
-          </div>
-        </div>
-        {/* 상품4 */}
+        ))}
+        {/* 더보기 상품 */}
         <div className="border p-4 rounded-md relative">
           <div className=" blur-sm">
             {/* relative for positioning the timer */}
             <div className="absolute top-1 right-1 bg-deepblue2/20 text-deepblue1 text-md px-2 py-1 rounded">
-              00:45:15
+              99:01:07
             </div>
             {/* 타이머 */}
             <img
