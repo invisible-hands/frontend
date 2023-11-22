4033e60dec1ac7a7a177d965d53d015ce0d170d6
HYUNJOON KWAK <deartomefirst@gmail.com>
2023-11-22 12:37:34 +0900

    feat: add conditional button rendering & css

--- src/components/PaymentModal.jsx
+++ src/components/PaymentModal.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   TERipple,
   TEModal,
@@ -10,7 +10,34 @@ import {
 } from 'tw-elements-react';
 import PropTypes from 'prop-types';
 
-export default function PaymentModal({ showModal, setShowModal }) {
+export default function PaymentModal({
+  showModal,
+  setShowModal,
+  money,
+  price,
+}) {
+  const [newPoint, setNewPoint] = useState(Number(price) + 1000);
+  const [pointError, setPointError] = useState(false);
+
+  const handleChange = e => {
+    const inputValue = e.target.value;
+    const regExp = /^[0-9\b]+$/;
+    if (regExp.test(inputValue)) {
+      setNewPoint(inputValue);
+    }
+
+    if (Number(inputValue) < Number(price) + 1000) {
+      setPointError(true);
+    } else {
+      setPointError(false);
+    }
+  };
+
+  useEffect(() => {
+    setNewPoint(Number(price) + 1000);
+    console.log('값 초기화');
+  }, [showModal]);
+
   return (
     <div className="flex justify-center items-center min-h-full place-items-center">
       <TEModal show={showModal} setShow={setShowModal}>
@@ -25,7 +52,9 @@ export default function PaymentModal({ showModal, setShowModal }) {
               <button
                 type="button"
                 className="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
-                onClick={() => setShowModal(false)}
+                onClick={() => {
+                  setShowModal(false);
+                }}
                 aria-label="Close"
               >
                 <svg
@@ -45,17 +74,70 @@ export default function PaymentModal({ showModal, setShowModal }) {
               </button>
             </TEModalHeader>
             {/* <!--Modal body--> */}
-            <TEModalBody>Modal body text goes here.</TEModalBody>
+            <TEModalBody>
+              <div className="flex justify-center">
+                <div className="w-96 space-y-5">
+                  <h5 className="text-2xl font-extrabold mb-4">
+                    충전 금액을 입력해주세요
+                  </h5>
+                  <div>
+                    <p>필요한 포인트 {price + 1000 - money} point</p>
+                    <p>충전 후 포인트 {Number(newPoint) + money} point</p>
+                  </div>
+                  <div>
+                    {pointError && (
+                      <p className="text-sm text-danger">{`추가적인 포인트 충전이 필요합니다 (최소 ${
+                        price + 1000
+                      } point 이상)`}</p>
+                    )}
+                    <input
+                      type="number"
+                      className="w-full bg-deepblue1 text-white px-2 py-1 rounded"
+                      value={newPoint}
+                      onChange={e => {
+                        handleChange(e);
+                      }}
+                    />
+                  </div>
+                  <div className="flex justify-between space-x-2 my-5">
+                    <TERipple rippleColor="light" rippleCentered>
+                      <button
+                        type="button"
+                        className="inline-block rounded bg-blackish px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-whitish transition duration-150 ease-in-out hover:bg-blue3 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200"
+                        onClick={() =>
+                          setNewPoint(state => Number(state) + 20000)
+                        }
+                      >
+                        +20000
+                      </button>
+                    </TERipple>
+                    <TERipple rippleColor="light" rippleCentered>
+                      <button
+                        type="button"
+                        className="inline-block rounded bg-blackish px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-whitish transition duration-150 ease-in-out hover:bg-blue3 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200"
+                        onClick={() =>
+                          setNewPoint(state => Number(state) + 50000)
+                        }
+                      >
+                        +50000
+                      </button>
+                    </TERipple>
+                    <TERipple rippleColor="light" rippleCentered>
+                      <button
+                        type="button"
+                        className="inline-block rounded bg-blackish px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-whitish transition duration-150 ease-in-out hover:bg-blue3 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200"
+                        onClick={() =>
+                          setNewPoint(state => Number(state) + 100000)
+                        }
+                      >
+                        +100000
+                      </button>
+                    </TERipple>
+                  </div>
+                </div>
+              </div>
+            </TEModalBody>
             <TEModalFooter className="flex justify-center">
-              <TERipple rippleColor="light" rippleCentered>
-                <button
-                  type="button"
-                  className="inline-block rounded bg-blue2 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-whitish transition duration-150 ease-in-out hover:bg-blue3 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200"
-                  onClick={() => setShowModal(false)}
-                >
-                  충전하기
-                </button>
-              </TERipple>
               <TERipple rippleColor="light" rippleCentered>
                 <button
                   type="button"
@@ -75,4 +157,6 @@ export default function PaymentModal({ showModal, setShowModal }) {
 PaymentModal.propTypes = {
   showModal: PropTypes.bool.isRequired,
   setShowModal: PropTypes.func.isRequired,
+  money: PropTypes.number.isRequired,
+  price: PropTypes.number.isRequired,
 };
