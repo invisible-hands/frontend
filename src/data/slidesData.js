09e2ba53c3a36a310d5b6cbc41268f294903ca13
GHeeJeon <jhjeon_99@naver.com>
2023-12-18 03:26:50 +0900

    chore: add banner images

--- src/data/slidesData.js
+++ src/data/slidesData.js
@@ -1,26 +1,30 @@
-import bannerImg from '../assets/banner.jpg';
+import bannerMain from '../assets/banner-main.jpg';
+import bannerHot from '../assets/banner-hot.jpg';
+import bannerDeadline from '../assets/banner-deadline.jpg';
+import bannerNew from '../assets/banner-new.jpg';
+import bannerUpload from '../assets/banner-upload.jpg';
 
 const slidesData = [
   {
     id: 0,
-    url: bannerImg,
+    url: bannerMain,
   },
   {
     id: 1,
-    url: 'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
+    url: bannerHot,
   },
   {
     id: 2,
-    url: 'https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80',
+    url: bannerDeadline,
   },
 
   {
     id: 3,
-    url: 'https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80',
+    url: bannerNew,
   },
   {
     id: 4,
-    url: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80',
+    url: bannerUpload,
   },
 ];
 
